a2af8acdc3e2c8808b054f04e2ce35724de75dba
Dmitriy <dimgod80@yandex.ru>
2022-10-27 12:36:26 +0800

    add Exercise10

---
+++ Exercise10/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int SecondDigit(int number)
+{
+    return ((number / 10) % 10);
+}
+
+void PrintSecondDigit(int number)
+{
+    Console.WriteLine(SecondDigit(number));
+}
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+//SecondDigit(number);
+PrintSecondDigit(number);
