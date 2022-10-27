e1d8a8655d9c2d43c4e20efa97d135b7614e0cd4
Dmitriy <dimgod80@yandex.ru>
2022-10-27 14:37:05 +0800

    add Ex13

---
+++ Exercise13/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void PrintThirdDigit (int number)
+{
+    if (number > 999)
+    {
+        while (number > 999)
+        {
+            number /= 10;
+        }
+        Console.WriteLine(number %= 10);
+    }
+    else if (number > 99 & number < 1000)
+    {
+        Console.WriteLine(number %= 10);
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не имеет третьей цифры.");
+    }
+}
+
+int number = new Random().Next(1, 100);
+Console.WriteLine(number);
+PrintThirdDigit(number);
