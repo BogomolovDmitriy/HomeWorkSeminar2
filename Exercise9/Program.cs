a69619997d13afc1f6bd016e4fc57c1cb71a9851
Dmitriy <dimgod80@yandex.ru>
2022-10-27 15:06:46 +0800

    add Exercise9

---
+++ Exercise9/Program.cs
@@ -0,0 +1,18 @@
+﻿//Дополнительная Задача 9
+//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+void MultiplicityOfNumber(int number)
+{
+    if (number % 7 == 0 && number % 23 == 0)
+    {
+        Console.WriteLine($"Число {number} кратно одновременно 7 и 23.");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} НЕ кратно 7 и 23 одновременно.");
+    }
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+MultiplicityOfNumber(number);
